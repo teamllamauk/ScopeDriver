cd9bc5f5485b678f2a4ceabad93631e56af403c6
teamllamauk <teamllamauk@users.noreply.github.com>
2017-05-16 12:50:01 +0100

    Create LSM303.py

---
+++ LSM303.py
@@ -0,0 +1,17 @@
+# I2C Addresses for device registers
+#
+# Check Device addresses (0x33/0x3D) with 'i2cdetect -y 0' in shell
+#
+# Compass: 0x3D ??? 
+# Compass X: 0x03 & 0x04
+# Compass Y: 0x05 & 0x06
+# Compass Z: 0x07 & 0x08
+#
+# Lin Accel: 0x33 ???
+# Lin Accel X: 0x28 & 0x29
+# Lin Accel Y: 0x2A & 0x2B
+# Lin Accel Z: 0x2C & 0x2D
+#
+# Temp: 0x3D ???
+# Temp: 0x31 & 0x32
+#
