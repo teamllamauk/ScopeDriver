b1a67b2e149d7ac0c3aed3a5ca7d46d7aa959bd7
teamllamauk <teamllamauk@users.noreply.github.com>
2017-05-17 14:03:23 +0100

    Create basicDrive.py

---
+++ basicDrive.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: Latin-1
+
+#pre-req : sudo apt-get -y install joystick
+#pre-req : jstest /dev/input/js0
+
+# Button 0: X
+# Button 1: A
+# Button 2: B
+# Button 3: Y
+# Button 4: L
+# Button 5: R
+
+# Button 8: Select
+# Button 9: Start
+
+
+# Load library functions we want
+import time
+import pygame
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+
+# Settings for JoyBorg
+axisUpDown = 1                          # Joystick axis to read for up / down position
+axisUpDownInverted = False              # Set this to True if up and down appear to be swapped
+axisLeftRight = 0                       # Joystick axis to read for left / right position
+axisLeftRightInverted = False           # Set this to True if left and right appear to be swapped
+interval = 0.1                          # Time between keyboard updates in seconds, smaller responds faster but uses more processor time
+
+# Setup pygame and key states
+global hadEvent
+global moveUp
+global moveDown
+global moveLeft
+global moveRight
+global moveQuit
+hadEvent = True
+moveUp = False
+moveDown = False
+moveLeft = False
+moveRight = False
+moveQuit = False
+pygame.init()
+pygame.joystick.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+screen = pygame.display.set_mode([300,300])
+pygame.display.set_caption("JoyBorg - Press [ESC] to quit")
+
+# Function to handle pygame events
+def PygameHandler(events):
+    # Variables accessible outside this function
+    global hadEvent
+    global moveUp
+    global moveDown
+    global moveLeft
+    global moveRight
+    global moveQuit
+    # Handle each event individually
+    for event in events:
+        if event.type == pygame.QUIT:
+            # User exit
+            hadEvent = True
+            moveQuit = True
+            
+        elif event.type == pygame.KEYDOWN:
+            # A key has been pressed, see if it is one we want
+            hadEvent = True
+            if event.key == pygame.K_ESCAPE:
+                moveQuit = True
+                
+        elif event.type == pygame.KEYUP:
+            # A key has been released, see if it is one we want
+            hadEvent = True
+            if event.key == pygame.K_ESCAPE:
+                moveQuit = False
+                
+        elif event.type == pygame.JOYBUTTONDOWN:
+            button = event.button
+            print("Button {} on".format(button))
+            
+        elif event.type == pygame.JOYBUTTONUP:
+            button = event.button
+            print("Button {} off".format(button))
+            
+        elif event.type == pygame.JOYAXISMOTION:
+            # A joystick has been moved, read axis positions (-1 to +1)
+            hadEvent = True
+            upDown = joystick.get_axis(axisUpDown)
+            leftRight = joystick.get_axis(axisLeftRight)
+            # Invert any axes which are incorrect
+            if axisUpDownInverted:
+                upDown = -upDown
+            if axisLeftRightInverted:
+                leftRight = -leftRight
+            # Determine Up / Down values
+            if upDown < -0.5:
+                print 'Move Up'
+            elif upDown > 0.5:
+                print 'Move Down'          
+                
+            # Determine Left / Right values
+            if leftRight < -0.5:
+                print 'Move Left'
+            elif leftRight > 0.5:
+                print 'Move Right'            
+                
+try:
+    print 'Press [ESC] to quit'
+    # Loop indefinitely
+    while True:
+        # Get the currently pressed keys on the keyboard
+        PygameHandler(pygame.event.get())                 
+        # Wait for the interval period
+        time.sleep(interval)
+except:        
+    print 'Error'
