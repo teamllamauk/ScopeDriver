2c8195dd9561727ca04ee2152966c02f9db4522d
teamllamauk <teamllamauk@users.noreply.github.com>
2017-05-24 11:52:08 +0100

    Update LSM303.py

--- LSM303.py
+++ LSM303.py
@@ -25,13 +25,7 @@ while True:
     mag_x, mag_z, mag_y = mag
     print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
           accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
-    
-    Axn = accel_x / math.sqrt(accel_x**2 + accel_y**2 + accel_z**2)
-    Ayn = accel_y / math.sqrt(accel_x**2 + accel_y**2 + accel_z**2)
-    
-    pitch = math.asin(-Axn)
-    roll = math.asin(Ayn / math.cos(pitch))
-    
+       
     #Acc Mins: -1290, -1306, -1017
     #Acc Maxs: 1010, 1173, 1217
     #Mag Mins: -767, -556, -1018
@@ -46,23 +40,37 @@ while True:
     MminZ = -1018
     MmaxZ = 670
     
+    Axn = accel_x / math.sqrt(accel_x * accel_x + accel_y * accel_y + accel_z * accel_z)
+    Ayn = accel_y / math.sqrt(accel_x * accel_x + accel_y * accel_y + accel_z * accel_z)
+    
+    pitch = math.asin(Axn)
+    roll = -math.asin(Ayn / math.cos(pitch))
+    
     Mxc = (mag_x - MminX) / (MmaxX - MminX) * 2 - 1
     Myc = (mag_y - MminY) / (MmaxY - MminY) * 2 - 1
     Mzc = (mag_z - MminZ) / (MmaxZ - MminZ) * 2 - 1
     
-    Mxh = Mxc * math.cos(pitch) + Mzc * math.sin(pitch)
-    Myh = Mxc * math.sin(roll) * math.sin(pitch) + Myc * math.cos(roll) - Mzc * math.sin(roll) * math.cos(pitch)
-        
+    magXcomp_cal = Mxc * math.cos(pitch) + Mzc * math.sin(pitch)
+    magYcomp_cal = Mxc * math.sin(roll) * math.sin(pitch) + Myc * math.cos(roll) - Mzc * math.sin(roll) * math.cos(pitch)
+    
+    magXcomp = mag_x * math.cos(pitch) + mag_z * math.sin(pitch)
+    magYcomp = mag_x * math.sin(roll) * math.sin(pitch) + mag_y * math.cos(roll) - mag_z * math.sin(roll) * math.cos(pitch)
+    
     heading = (math.atan2(mag_y, mag_x) * 180) / math.pi
     if heading < 0:
         heading = 360 + heading
         
-    headingTilt = (math.atan2(Mxh, Mxh) * 180) / math.pi
+    headingTilt = (math.atan2(magYcomp, magXcomp) * 180) / math.pi
     if headingTilt < 0:
         headingTilt = 360 + headingTilt
         
+    headingTiltCal = (math.atan2(magYcomp_cal, magXcomp_cal) * 180) / math.pi
+    if headingTiltCal < 0:
+        headingTiltCal = 360 + headingTiltCal
+        
     print('Compass heading: {0}'.format(heading))
     print('Tilt Compass heading: {0}'.format(headingTilt))
+    print('Cal Tilt Compass heading: {0}'.format(headingTiltCal))
     
     # Wait half a second and repeat.
     time.sleep(0.5)
