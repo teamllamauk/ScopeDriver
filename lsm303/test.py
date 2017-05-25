816d0a095e9b4c0efbb0916992ec1decdde82c26
teamllamauk <teamllamauk@users.noreply.github.com>
2017-05-25 07:16:34 +0100

    Update test.py

--- lsm303/test.py
+++ lsm303/test.py
@@ -1,6 +1,6 @@
-from functions_lsm303 import *
+from functions_lsm303 import mpd_LSM303
 
-heading = functions_lsm303.bearing()
-tilt = functions_lsm303.inclination()
+heading = mpd_LSM303.bearing()
+tilt = mpd_LSM303.inclination()
 
 print('Heading: {0}, Tilt: {1}'.format(heading,tilt))
