3bf7f137454571abb764cd28a55e3fe65f004cb1
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-01 15:46:47 +0100

    Update display.py

--- lsm303/display.py
+++ lsm303/display.py
@@ -1,3 +1,4 @@
+import time
 import dothat.backlight as backlight
 import dothat.lcd as lcd
 import functions_lsm303
@@ -5,14 +6,19 @@ import functions_lsm303
 
 compass = functions_lsm303.functions_lsm303()
 
-heading = compass.bearing()
-tilt = compass.inclination()
-
-lcd.clear()
+lcd.clear()    
 backlight.rgb(255, 0, 0)
 
-lcd.set_cursor_position(0,0)
-lcd.write('H: {0}'.format(heading))
+while True:
+    heading = compass.bearing()
+    tilt = compass.inclination()
 
-lcd.set_cursor_position(9,0)
-lcd.write('T: {0}'.format(tilt))
+    lcd.clear()
+    
+    lcd.set_cursor_position(0,0)
+    lcd.write('H: {0}'.format(heading))
+
+    lcd.set_cursor_position(9,0)
+    lcd.write('T: {0}'.format(tilt))
+
+    time.sleep(0.1)
