a314ad702b969d3524544b4eb5b81beee21a26f1
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-01 16:29:31 +0100

    Update functions_lsm303.py

--- lsm303/functions_lsm303.py
+++ lsm303/functions_lsm303.py
@@ -38,10 +38,10 @@ class functions_lsm303():
         
         self.mag_Xcal = (self.mag_X - self.mag_Xmin) / (self.mag_Xmax - self.mag_Xmin) * 2 - 1
         self.mag_Ycal = (self.mag_Y - self.mag_Ymin) / (self.mag_Ymax - self.mag_Ymin) * 2 - 1
-        #self.mag_Zcal = (self.mag_Z - self.mag_Zmin) / (self.mag_Zmax - self.mag_Zmin) * 2 - 1
+        self.mag_Zcal = (self.mag_Z - self.mag_Zmin) / (self.mag_Zmax - self.mag_Zmin) * 2 - 1
         
-        #self.mag_Xcal = self.mag_X
-        #self.mag_Ycal = self.mag_Y
+        self.mag_Xcal = self.mag_X
+        self.mag_Ycal = self.mag_Y
         self.mag_Zcal = self.mag_Z
         
         self.mag_Xtilt = self.mag_Xcal * math.cos(self.pitch) + self.mag_Zcal * math.sin(self.pitch)
