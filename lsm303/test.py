7174c50d232a1fb0a563b166dc6432b27a7853b2
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-02 13:16:49 +0100

    Update test.py

--- lsm303/test.py
+++ lsm303/test.py
@@ -2,10 +2,15 @@ import functions_lsm303
 
 compass = functions_lsm303.functions_lsm303()
 
+accRaw, magRaw = compass.rawData()
+
+
 heading, headingCal, headingTilt = compass.bearing()
 altHeadingA = compass.bearingAltA()
 tilt = compass.inclination()
 
+print('Raw Acc Data: {0}'.format(accRaw))
+print('Raw Mag Data: {0}'.format(magRaw))
 print('Heading: {0}'.format(heading))
 print('Heading Cal: {0}'.format(headingCal))
 print('Heading Tilt: {0}'.format(headingTilt))
