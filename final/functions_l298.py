8024795d4d573ea946f1cea44cc0c5ac296ec473
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-06 09:53:37 +0100

    Update functions_l298.py

--- final/functions_l298.py
+++ final/functions_l298.py
@@ -1,1 +1,17 @@
+import time
+import math
 
+
+
+class functions_l298():
+    
+    def __init__(self):
+        #set init values
+        
+    
+    def rawData(self):
+        
+        self.acc, self.mag = lsm303.read()
+        
+        return (self.acc, self.mag)
+   
