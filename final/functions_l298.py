2ff584cd5f534a7005e7a8da8c42688bc521a024
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-06 11:15:11 +0100

    Update functions_l298.py

--- final/functions_l298.py
+++ final/functions_l298.py
@@ -11,6 +11,9 @@ class functions_l298():
         self.delay = 0.0055
         self.steps = 10
         
+        self.drivingUpDown = False
+        self.drivingLeftRight = False
+        
         # RA Motor Pins
         self.RA_coil_A_1_pin = 17
         self.RA_coil_A_2_pin = 18
@@ -33,52 +36,50 @@ class functions_l298():
         GPIO.setup(self.DEC_coil_B_1_pin, GPIO.OUT)
         GPIO.setup(self.DEC_coil_B_2_pin, GPIO.OUT)
         
-    def driveMotor(motor, direction):
+    def driveMotor(self, motor, direction):
+               
         if motor == 1:
-            drivingUpDown = True
+            self.drivingUpDown = True
         else:
-            drivingLeftRight = True
+            self.drivingLeftRight = True
     
         if direction == 1: # Forward
-            setStep(motor,1,0,1,0)
-            time.sleep(delay)
-            setStep(motor,0,1,1,0)
-            time.sleep(delay)
-            setStep(motor,0,1,0,1)
-            time.sleep(delay)
-            setStep(motor,1,0,0,1)
-            time.sleep(delay)
+            self.setStep(motor,1,0,1,0)
+            time.sleep(self.delay)
+            self.setStep(motor,0,1,1,0)
+            time.sleep(self.delay)
+            self.setStep(motor,0,1,0,1)
+            time.sleep(self.delay)
+            self.setStep(motor,1,0,0,1)
+            time.sleep(self.delay)
         else: # Reverse
-            setStep(motor,1,0,0,1)
-            time.sleep(delay)
-            setStep(motor,0,1,0,1)
-            time.sleep(delay)
-            setStep(motor,0,1,1,0)
-            time.sleep(delay)
-            setStep(motor,1,0,1,0)
-            time.sleep(delay)
+            self.setStep(motor,1,0,0,1)
+            time.sleep(self.delay)
+            self.setStep(motor,0,1,0,1)
+            time.sleep(self.delay)
+            self.setStep(motor,0,1,1,0)
+            time.sleep(self.delay)
+            self.setStep(motor,1,0,1,0)
+            time.sleep(self.delay)
         
-        if motor == 1:
-            drivingUpDown = False
+        if self.motor == 1:
+            self.drivingUpDown = False
         else:
-            drivingLeftRight = False
+            self.drivingLeftRight = False
 
     # Function for step sequence
-    def setStep(motor, w1, w2, w3, w4):
+    def setStep(self, motor, w1, w2, w3, w4):
+        
         if motor == 1: # Dec
-            GPIO.output(DEC_coil_A_1_pin, w1)
-            GPIO.output(DEC_coil_A_2_pin, w2)
-            GPIO.output(DEC_coil_B_1_pin, w3)
-            GPIO.output(DEC_coil_B_2_pin, w4)
+            GPIO.output(self.DEC_coil_A_1_pin, w1)
+            GPIO.output(self.DEC_coil_A_2_pin, w2)
+            GPIO.output(self.DEC_coil_B_1_pin, w3)
+            GPIO.output(self.DEC_coil_B_2_pin, w4)
         else: # RA
-            GPIO.output(RA_coil_A_1_pin, w1)
-            GPIO.output(RA_coil_A_2_pin, w2)
-            GPIO.output(RA_coil_B_1_pin, w3)
-            GPIO.output(RA_coil_B_2_pin, w4)
+            GPIO.output(self.RA_coil_A_1_pin, w1)
+            GPIO.output(self.RA_coil_A_2_pin, w2)
+            GPIO.output(self.RA_coil_B_1_pin, w3)
+            GPIO.output(self.RA_coil_B_2_pin, w4)
     
-    def rawData(self):
-        
-        self.acc, self.mag = lsm303.read()
-        
-        return (self.acc, self.mag)
-   
+  
+
