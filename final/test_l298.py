635e4e885bbbe665c375435f85dbd278539fef60
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-06 14:49:13 +0100

    Update test_l298.py

--- final/test_l298.py
+++ final/test_l298.py
@@ -6,16 +6,16 @@ L298 = functions_l298.functions_l298('0.0055', '10')
 while True:
     print('Drive 0, 0')
     L298.driveMotor(0, 0)
-    time.sleep(1000)
+    time.sleep(2)
     
     print('Drive 0, 1')
     L298.driveMotor(0, 1)
-    time.sleep(1000)
+    time.sleep(2)
     
     print('Drive 1, 0')
     L298.driveMotor(1, 0)
-    time.sleep(1000)
+    time.sleep(2)
     
     print('Drive 1, 1')
     L298.driveMotor(1, 1)
-    time.sleep(1000)
+    time.sleep(2)
