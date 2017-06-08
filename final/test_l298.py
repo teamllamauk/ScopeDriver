42864bfea8bf4ca5298116014464b8acc36c2533
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-08 16:02:15 +0100

    Create test_l298.py

--- final/test_l298.py
+++ final/test_l298.py
@@ -1,13 +1,19 @@
 import time
+import threading
 import functions_l298
 
 L298 = functions_l298.functions_l298('0.0055', '40')
 L298.setupGPIO(17, 18, 22, 23, 1, 5, 6, 13)
 
 while True:
-    print('Drive 0, 0')
-    L298.driveMotor(0, 0)
+    
+    print('Thread Drive 0, 0')
+    t = threading.Thread(target=L298.driveMotor,args=(0,0))
+    t.start()
     time.sleep(2)
+    
+    
+    
     
     print('Drive 0, 1')
     L298.driveMotor(0, 1)
