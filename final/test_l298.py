3f9f766c028c42f8130339980bce991b3a1aeaf2
teamllamauk <teamllamauk@users.noreply.github.com>
2017-06-08 15:52:51 +0100

    Create test_l298.py

--- final/test_l298.py
+++ final/test_l298.py
@@ -2,6 +2,7 @@ import time
 import functions_l298
 
 L298 = functions_l298.functions_l298('0.0055', '40')
+L298.setupGPIO(17, 18, 22, 23, 1, 5, 6, 13)
 
 while True:
     print('Drive 0, 0')
@@ -18,4 +19,20 @@ while True:
     
     print('Drive 1, 1')
     L298.driveMotor(1, 1)
+    time.sleep(2)
+    
+    print('Half Drive 0, 0')
+    L298.halfStepDriveMotor(0, 0)
+    time.sleep(2)
+    
+    print('Half Drive 0, 1')
+    L298.halfStepDriveMotor(0, 1)
+    time.sleep(2)
+    
+    print('Half Drive 1, 0')
+    L298.halfStepDriveMotor(1, 0)
+    time.sleep(2)
+    
+    print('Half Drive 1, 0')
+    L298.halfStepDriveMotor(1, 0)
     time.sleep(2)
