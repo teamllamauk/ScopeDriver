059a0458d2495417f946f2e67be61172108453ab
teamllamauk <teamllamauk@users.noreply.github.com>
2017-07-19 13:42:02 +0100

    Update raspeedtest.py

--- raspeedtest.py
+++ raspeedtest.py
@@ -4,18 +4,18 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 
-lcd.clear()
-backlight.rgb(255, 0, 0)
-
-lcd.set_cursor_position(0,0)
-lcd.write("Delay")
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 global delay 
 delay = 0.0012
 
+#Setup LCD
+lcd.clear()
+backlight.rgb(255, 0, 0)
+
+lcd.set_cursor_position(0,0)
+lcd.write("Delay")
 
 lcd.set_cursor_position(0,1)
 lcd.write(str(delay))
@@ -23,113 +23,53 @@ lcd.write(str(delay))
 lcd.set_cursor_position(0,2)
 lcd.write("X inc - Y dec")
 
+# Pin assignments
 coil_A_1_pin = 17
 coil_A_2_pin = 18
 coil_B_1_pin = 22
 coil_B_2_pin = 23
 
+btn_red_pin = 1
+btn_green_pin = 1
+btn_blue_pin = 1
+btn_yellow_pin = 1
+btn_black_top_pin = 1
+btn_black_bottom_pin = 1
+
+#GPIO outputs
 GPIO.setup(coil_A_1_pin, GPIO.OUT)
 GPIO.setup(coil_A_2_pin, GPIO.OUT)
 GPIO.setup(coil_B_1_pin, GPIO.OUT)
 GPIO.setup(coil_B_2_pin, GPIO.OUT)
 
+#GPIO inputs
+GPIO.setup(btn_red_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_red_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
 
+GPIO.setup(btn_green_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_green_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
 
-# Settings for JoyBorg
-axisUpDown = 1                          # Joystick axis to read for up / down position
-axisUpDownInverted = False              # Set this to True if up and down appear to be swapped
-axisLeftRight = 0                       # Joystick axis to read for left / right position
-axisLeftRightInverted = False           # Set this to True if left and right appear to be swapped
-interval = 0.1                          # Time between keyboard updates in seconds, smaller responds faster but uses more processor time
+GPIO.setup(btn_blue_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_blue_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
 
-# Setup pygame and key states
-global hadEvent
-global moveUp
-global moveDown
-global moveLeft
-global moveRight
-global moveQuit
-hadEvent = True
-moveUp = False
-moveDown = False
-moveLeft = False
-moveRight = False
-moveQuit = False
-pygame.init()
-pygame.joystick.init()
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-screen = pygame.display.set_mode([300,300])
-pygame.display.set_caption("JoyBorg - Press [ESC] to quit")
+GPIO.setup(btn_yellow_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_yellow_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
 
-# Function to handle pygame events
-def PygameHandler(events):
-    
-    #print('pygame')
-    # Variables accessible outside this function
-    global hadEvent
-    global moveUp
-    global moveDown
-    global moveLeft
-    global moveRight
-    global moveQuit
+GPIO.setup(btn_black_top_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_black_top_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
+
+GPIO.setup(btn_black_bottom_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_black_bottom_pin, GPIO.RISING, callback=btn_Callback, bouncetime=200)
+
+#Functions
+def btn_Callback(button_pin):
     
     global delay
     
-    # Handle each event individually
-    for event in events:
-        if event.type == pygame.QUIT:
-            # User exit
-            hadEvent = True
-            moveQuit = True
-        elif event.type == pygame.KEYDOWN:
-            # A key has been pressed, see if it is one we want
-            hadEvent = True
-            if event.key == pygame.K_ESCAPE:
-                moveQuit = True
-        elif event.type == pygame.KEYUP:
-            # A key has been released, see if it is one we want
-            hadEvent = True
-            if event.key == pygame.K_ESCAPE:
-                moveQuit = False
-        elif event.type == pygame.JOYBUTTONDOWN:
-            print('button')
-            button = event.button
-            if button == 0:
-                delay = delay + 0.0001
-                print('0')
-                print(delay)
-            elif button == 3:
-                delay = delay - 0.0001
-                print('3')
-                print(delay)
-                
-        elif event.type == pygame.JOYBUTTONUP:
-            button = event.button
-            
-        elif event.type == pygame.JOYAXISMOTION:
-            # A joystick has been moved, read axis positions (-1 to +1)
-            hadEvent = True
-            upDown = joystick.get_axis(axisUpDown)
-            leftRight = joystick.get_axis(axisLeftRight)
-            # Invert any axes which are incorrect
-            if axisUpDownInverted:
-                upDown = -upDown
-            if axisLeftRightInverted:
-                leftRight = -leftRight
-                # Determine Up / Down values
-                #if upDown < -0.5:
-                #print 'Move Up'
-                #elif upDown > 0.5:
-                #print 'Move Down'          
-                
-                # Determine Left / Right values
-                #if leftRight < -0.5:
-                #print 'Move Left'
-                #elif leftRight > 0.5:
-                #print 'Move Right'  
-
-
+    if button_pin == btn_red_pin:
+        delay = delay + 0.0001
+    elif button_pin == btn_yellow_pin:
+        delay = delay - 0.0001
 
 
 def setStep(w1, w2, w3, w4):
@@ -138,9 +78,9 @@ def setStep(w1, w2, w3, w4):
     GPIO.output(coil_B_1_pin, w3)
     GPIO.output(coil_B_2_pin, w4)
 
+#Main loop
 while True:
-    PygameHandler(pygame.event.get())
-    
+        
     lcd.set_cursor_position(0,1)
     lcd.write(str(delay))
     
