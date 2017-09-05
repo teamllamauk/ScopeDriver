b98d2efad7e2e71542c99aabc1f375482a7eaaed
teamllamauk <teamllamauk@users.noreply.github.com>
2017-09-05 07:02:46 +0100

    Update loadProgramFromButton.py

--- loadProgramFromButton.py
+++ loadProgramFromButton.py
@@ -15,16 +15,8 @@ import fcntl
 import socket
 import struct
 
-def get_addr(ifname):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(
-            s.fileno(),
-            0x8915,  # SIOCGIFADDR
-            struct.pack('256s', ifname[:15].encode('utf-8'))
-        )[20:24])
-    except IOError:
-        return 'Not Found!'
+global loadOnButton
+loadOnButton = 0
 
 GPIO.setmode(GPIO.BCM)  
 
@@ -37,17 +29,46 @@ lcd.write("Ready...")
 lcd.set_cursor_position(0, 1)
 lcd.write("Top Blk btn")
 
-lcd.set_cursor_position(0, 2)
-lcd.write(get_addr('wlan0'))
+
 
 btn_black_top_pin = 16
 
-GPIO.setup(btn_black_top_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+def get_addr(ifname):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15].encode('utf-8'))
+        )[20:24])
+    except IOError:
+        return 'Not Found!'
+
+
+def btn_Callback(button_pin):
+    
+    global loadOnButton
+    
+    if button_pin == btn_black_top_pin:
+        loadOnButton = 1
+
+
+        
+# GPIO.setup(btn_black_top_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
  
+GPIO.setup(btn_black_top_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_black_top_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+lcd.set_cursor_position(0, 2)
+lcd.write(get_addr('wlan0'))
+
+while True:
 #try:  
-GPIO.wait_for_edge(btn_black_top_pin, GPIO.FALLING)
-print("Button Pressed")
-subprocess.call(["sudo", "python", "/home/pi/ScopeDriver/final/scopedriver.py"])
+#GPIO.wait_for_edge(btn_black_top_pin, GPIO.FALLING)
+if loadOnButton = 1:
+    print("Button Pressed")
+    subprocess.call(["sudo", "python", "/home/pi/ScopeDriver/final/scopedriver.py"])
+    break
 # except KeyboardInterrupt:  
 #    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
-GPIO.cleanup()           # clean up GPIO on normal exit  
+#GPIO.cleanup()           # clean up GPIO on normal exit  
