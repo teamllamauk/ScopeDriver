89ef5f82c196454758634f4aa0c4f4331c3287a0
teamllamauk <teamllamauk@users.noreply.github.com>
2017-09-05 10:59:28 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/ScopeDriver
+cd home/pi/ScopeDriver
 sudo python loadProgramFromButton.py
 cd /
