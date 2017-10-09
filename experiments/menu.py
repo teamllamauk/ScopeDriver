e5321ccfd8e2d93cbd2950c5c2c317fb8d9eb1b8
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-09 15:58:34 +0100

    Update menu.py

--- experiments/menu.py
+++ experiments/menu.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import RPi.GPIO as GPIO
 
 import dothat.backlight as backlight
 import dothat.lcd as lcd
@@ -17,6 +18,47 @@ from plugins.text import Text
 #from plugins.utils import Backlight, Contrast
 #from plugins.wlan import Wlan
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Setup LCD
+lcd.clear()
+backlight.rgb(255, 0, 0)
+
+btn_red_pin = 27            # A
+btn_green_pin = 24          # Y
+btn_blue_pin = 9            # X
+btn_yellow_pin = 19         # B
+btn_black_top_pin = 16      # purple wire
+btn_black_bottom_pin = 26   # grey wire
+
+# Callback Functions
+def btn_Callback(button_pin):
+    if button_pin == btn_green_pin:
+        menu.select_option()        
+    elif button_pin == btn_black_top_pin:
+        menu.up()
+    elif button_pin == btn_black_top_pin:
+        menu.down()
+
+
+GPIO.setup(btn_red_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_red_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+GPIO.setup(btn_green_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_green_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+GPIO.setup(btn_blue_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_blue_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+GPIO.setup(btn_yellow_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_yellow_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+GPIO.setup(btn_black_top_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_black_top_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
+
+GPIO.setup(btn_black_bottom_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(btn_black_bottom_pin, GPIO.RISING, callback=btn_Callback, bouncetime=300)
 
 print("""
 This advanced example uses the menu framework.
@@ -35,19 +77,9 @@ See GraphTemp, GraphCPU, Contrast and Backlight for examples.
 
 menu = Menu(
     structure={
-        'WiFi': '1',        
-        'Clock': '2',
-        'Status': {
-            'IP': '1.2.3.4',
-            'CPU': 'graph',
-            'Temp': 'temp'
-        },
-        'Settings': {
-            'Display': {
-                'Contrast': 'lcd',
-                'Backlight': 'back'
-            }
-        }
+        'Setup': '1',        
+        'Tracking': '2',
+        'Exit': '3'
     },
     lcd=lcd,    
     input_handler=Text())
@@ -58,7 +90,6 @@ but you'll probably want to use dot3k.touch
 """
 #nav.bind_defaults(menu)
 
-while 1:
-    menu.down()
+while 1:    
     menu.redraw()
-    time.sleep(3)
+    time.sleep(0.05)
