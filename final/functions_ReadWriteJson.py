d8272935c7af7a6b95130218dfb36623da5bcaba
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-16 16:10:15 +0100

    Create functions_ReadWriteJson.py

---
+++ final/functions_ReadWriteJson.py
@@ -0,0 +1,28 @@
+import os.path
+import json
+
+class functions_ReadWriteJson():
+
+    def __init__(self):
+                
+        settingsFile = Path("ScopeSettings.json")
+        
+        try:
+            settingsAbsPath = settingsFile.resolve():
+        except FileNotFoundError:
+            data = {
+                "speed":"0.0012"
+            }
+            jsonData = json.dumps(data)
+            with open('JSONData.json', 'w') as f:
+                json.dump(jsonData, f)
+
+    def readJSON(self):
+        with open('JSONData.json', 'r') as f:
+            data = json.load(f)
+            return (data)
+    
+    def writeJSON(self, data):
+        jsonData = json.dumps(data)
+        with open('JSONData.json', 'w') as f:
+            json.dump(jsonData, f)
