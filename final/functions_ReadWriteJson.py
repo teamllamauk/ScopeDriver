04cf0326ba1e1b30007f4b9d7a8ae794578e6084
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-16 16:33:23 +0100

    Update functions_ReadWriteJson.py

--- final/functions_ReadWriteJson.py
+++ final/functions_ReadWriteJson.py
@@ -14,15 +14,15 @@ class functions_ReadWriteJson():
                 "speed":"0.0012"
             }
             jsonData = json.dumps(data)
-            with open('JSONData.json', 'w') as f:
+            with open('ScopeSettings.json', 'w') as f:
                 json.dump(jsonData, f)
 
     def readJSON(self):
-        with open('JSONData.json', 'r') as f:
+        with open('ScopeSettings.json', 'r') as f:
             data = json.load(f)
             return (data)
     
     def writeJSON(self, data):
         jsonData = json.dumps(data)
-        with open('JSONData.json', 'w') as f:
+        with open('ScopeSettings.json', 'w') as f:
             json.dump(jsonData, f)
