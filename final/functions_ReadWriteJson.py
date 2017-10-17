1ae041f3e43adedfb1079006936a8e3caa0d270f
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-17 13:07:26 +0100

    Update functions_ReadWriteJson.py

--- final/functions_ReadWriteJson.py
+++ final/functions_ReadWriteJson.py
@@ -8,26 +8,23 @@ class functions_ReadWriteJson():
                 
         settingsAbsPath =  os.path.abspath("ScopeSettings.json")
         
-        try:
-            settingsResolved = settingsAbsPath.resolve()
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-            data = {
-                "speed":"0.0012"
-            }
-            jsonData = json.dumps(data)
-            with open('ScopeSettings.json', 'w') as f:
-                json.dump(jsonData, f)
+        if os.path.isfile('data.json'):
+            print("Found file")
+        else:
+            print("File Not Found")
+            
+            data = {}
+            data['settings'] = []
+            data['settings'].append({'speed': 0.0012})            
+            with open('ScopeSettings.json', 'w') as json_file:
+                json.dump(data, json_file)
 
     def readJSON(self):
-        with open('ScopeSettings.json', 'r') as f:
-            rawData = str(f.readlines())
-            print(rawData)
-            data = json.load(rawData)
+        with open('ScopeSettings.json', 'r') as json_file:
+            data = json.load(json_file)
             print(data)
             return (data)
     
-    def writeJSON(self, data):
-        jsonData = json.dumps(data)
-        with open('ScopeSettings.json', 'w') as f:
-            json.dump(jsonData, f)
+    def writeJSON(self, data):        
+        with open('ScopeSettings.json', 'w') as json_file:
+            json.dump(data, json_file)
