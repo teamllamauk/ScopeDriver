0a45d33d8d1c5b82ca8291b26548c5e9b78fcf43
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-17 13:09:43 +0100

    Update testjson.py

--- final/testjson.py
+++ final/testjson.py
@@ -1,25 +1,32 @@
-import sys
 import os
 import json
 
-data = {}
-data['settings'] = []
-data['settings'].append({'speed': 0.0012})
+#data = {}
+#data['settings'] = []
+#data['settings'].append({'speed': 0.0012})
 
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+#with open('data.json', 'w') as outfile:
+#    json.dump(data, outfile)
 
 
-with open('data.json') as json_file:
-    data = json.load(json_file)
-    print(data)
-    print(data['settings'][0])
-    print(data['settings'][0]['speed'])
-    #for p in data['settings']:
-    #    print(p['speed'])
+#with open('data.json') as json_file:
+#    data = json.load(json_file)
+#    print(data)
+#    print(data['settings'][0])
+#    print(data['settings'][0]['speed'])
+#    #for p in data['settings']:
+#    #    print(p['speed'])
 
     
-if os.path.isfile('data.json'):
-    print("True")
-else:
-    print("False")
+#if os.path.isfile('data.json'):
+#    print("True")
+#else:
+#    print("False")
+
+    
+import functions_ReadWriteJson
+
+JSON_ReadWrite = functions_ReadWriteJson.functions_ReadWriteJson()
+
+JSON_settings = JSON_ReadWrite.readJSON()
+print(JSON_settings)
