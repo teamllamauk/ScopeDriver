d50e1afd2a14999e058a88f6da1bc7c1a5bb030d
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-17 11:53:55 +0100

    Update testjson.py

--- final/testjson.py
+++ final/testjson.py
@@ -1,16 +1,10 @@
-import functions_ReadWriteJson
+import sys
+import os
+import json
 
-global JSON_settings
+data = {}
+data['settings'] = []
+data['settings'].append({'speed': 0.0012})
 
-JSON_ReadWrite = functions_ReadWriteJson.functions_ReadWriteJson()
-
-JSON_settings = JSON_ReadWrite.readJSON()
-#print("Full JSON: ", JSON_settings)
-
-print(JSON_settings['speed'])
-
-#delayString = JSON_settings['speed']
-#print("JSON Speed String: ", delayString)
-
-#delay = int(delayString)
-#print("JSON Speed Int: ", delay)
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
