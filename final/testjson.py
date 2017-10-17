d719661dfcd149a3bc1055bc3b3b9d417315509b
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-17 11:59:54 +0100

    Update testjson.py

--- final/testjson.py
+++ final/testjson.py
@@ -8,3 +8,9 @@ data['settings'].append({'speed': 0.0012})
 
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
+
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('speed: ' + p['speed'])
