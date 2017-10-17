7f86c9ec844d43cfbd8e6340475e6cc9c0e821bd
teamllamauk <teamllamauk@users.noreply.github.com>
2017-10-17 11:19:29 +0100

    Update testjson.py

--- final/testjson.py
+++ final/testjson.py
@@ -5,10 +5,12 @@ global JSON_settings
 JSON_ReadWrite = functions_ReadWriteJson.functions_ReadWriteJson()
 
 JSON_settings = JSON_ReadWrite.readJSON()
-print("Ful JSON: ", JSON_settings)
+#print("Full JSON: ", JSON_settings)
 
-delayString = JSON_settings['speed']
-print("JSON Speed String: ", delayString)
+print(JSON_settings['speed'])
 
-delay = int(delayString)
-print("JSON Speed Int: ", delay)
+#delayString = JSON_settings['speed']
+#print("JSON Speed String: ", delayString)
+
+#delay = int(delayString)
+#print("JSON Speed Int: ", delay)
