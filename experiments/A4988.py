40e81a4891e6c0a944ca130339d5679062e9ac58
teamllamauk <teamllamauk@users.noreply.github.com>
2018-08-08 14:10:54 +0100

    Update A4988.py

--- experiments/A4988.py
+++ experiments/A4988.py
@@ -23,8 +23,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, 0)
+GPIO.setup(22, GPIO.OUT)
+GPIO.output(22, 0)
 
 #stepTime = 0.05
 stepTime = 2
